e04161c22702c43e2141ebfcc32233dab40d13c3
Sageto <maximesanmartin@gmail.com>
2016-10-22 16:56:55 +0200

    Creation de la page contact + début de la page d'accueil

---
+++ contact.php
@@ -0,0 +1,24 @@
+<html>
+<?php include("header.php"); ?>
+<link rel="stylesheet" type="text/css" href="css/contact.css">
+<body>
+<?php include("navbar.php"); ?>
+<div class="container contact">
+	<div class="col-md-8">
+		<h1>Nous contacter, tu peux !</h1>
+		<input class="input" type="text" placeholder="Pseudo"></input>
+		<textarea class="input" placeholder="Votre commentaire ici (Supporte le balisage HTML)"></textarea>
+		<br/>
+		<button class="btn btn-primary">Envoyer</button>
+	</div>
+	<div class="col-md-offset-1 col-md-3 sidebar">
+		<h2>Nous sommes aussi disponibles sur</h2>
+		<a href="https://www.facebook.com/NewsGeeksFLM/?fref=ts"><img src="img/facebook.png"/></a>
+		<a href="https://twitter.com/NewsGeeks_FLM"><img src="img/twitter.png"/></a>
+		<a href="https://www.youtube.com/user/SuperSageto"><img src="img/youtube.png"/></a>
+		<a href="https://www.twitch.tv/newsgeeks_gaming"><img src="img/twitch.png"/></a>
+	</div>
+</div>
+<?php include("footer.php"); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <html>
 <?php include("header.php"); ?>
+<link rel="stylesheet" type="text/css" href="css/accueil.css">
 <body>
 <?php include("navbar.php"); ?>
+<div class="container home">
+	<div class="col-md-8 articles">
+		<div class="col-md-6 article">
+		</div>
+		<div class="col-md-6 article">
+		</div>
+		<div class="col-md-6 article">
+		</div>
+		<div class="col-md-6 article">
+		</div>
+	</div>
+	<div class="col-md-4 sidebar">
+		<div class="col-md-12 twitter">
+			<h2>Actu Twitter</h2>
+			<a class="twitter-timeline" data-dnt="true" data-theme="light" data-link-color="#E95F28" href="https://twitter.com/NewsGeeks_FLM">Tweets de NewsGeeks_FLM</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+		</div>
+	</div>
+</div>
 <?php include("footer.php"); ?>
 </body>
 </html>
