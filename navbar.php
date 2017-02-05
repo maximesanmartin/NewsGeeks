35f6522b3e1a319487042c25060e593b087cb5f8
Sageto <maximesanmartin@gmail.com>
2017-02-05 12:38:03 +0100

    Ajout de la side navbar

--- navbar.php
+++ navbar.php
@@ -1,21 +1,17 @@
 <div class="head" id="top" style="background-image: url('<?php bloginfo( "template_directory" ); ?>/img/banner.jpg');">
 	<div id="title"><img src="<?php bloginfo( 'template_directory' ); ?>/img/logo.png">NewsGeeks</div>
 </div>
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-inverse" role="navigation" id="slide-nav">
 	<div class='container'>
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			<a class="navbar-toggle">
 				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
+				<i class="button-navbar fa fa-bars fa-2x" aria-hidden="true" style="color: white"></i>
+			</a>
+      <a class="navbar-brand" href="<?php bloginfo('url'); ?>">NewsGeeks</a>
 		</div>
-		<div id="navbar" class="collapse navbar-collapse">
+		<div id="slidemenu">
 			<ul class="nav navbar-nav">
-  				<li>
-  					<a href="<?php bloginfo('url')?>">ACCUEIL</a>
-  				</li>
           <?php foreach (get_categories(array("exclude" => 1)) as $category) { ?>
             <li>
               <a href="<?php echo get_category_link( $category->term_id )?>"><?php echo mb_strtoupper($category->name) ?></a>
@@ -23,6 +19,7 @@
           <?php } ?>
         </ul>
         <ul class="nav navbar-nav navbar-right">
+          <li class="divider"></li>
   				<li>
   					<a href="<?php bloginfo( 'url' ); ?>/contact">CONTACT</a>
   				</li>
